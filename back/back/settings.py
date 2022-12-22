de3e7c05d44d7bc91fafaebf82c53c88a842ac77
Chenjunjie <2624882033@qq.com>
2022-12-22 15:52:48 +0800

    cjj

--- back/back/settings.py
+++ back/back/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'club_system',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': '888718',
         'HOST': 'localhost',
         'PORT': '3306'
     }
