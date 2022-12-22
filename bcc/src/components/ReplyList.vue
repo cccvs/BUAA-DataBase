8f6bcbe11095bc7e07c12e8f490f36d316130c9d
Chenjunjie <2624882033@qq.com>
2022-12-22 17:44:39 +0800

    cjj:发布贴

--- bcc/src/components/ReplyList.vue
+++ bcc/src/components/ReplyList.vue
@@ -1,30 +1,82 @@
 <template>
   <v-app style="height:100%;overflow-y: hidden;overflow-x: hidden">
-    <v-list subheader>
+    <h1 style="margin-top: 10px;margin-bottom: 10px">讨论：{{ post.title }}</h1>
+    <v-card
+        class="mx-auto"
+        width="98%"
+    >
+      <v-card-title>
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+              class="elevation-6"
+              :src="post.avatar"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ post.user_name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ post.time }}</v-list-item-subtitle>
+          <v-row
+              align="center"
+              justify="end"
+              style="margin-right: 20px;"
+          >
+            <v-btn icon color="deep-orange">
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-btn>
+            <div>{{ post.like }}</div>
+            <v-btn icon color="blue-grey darken-2">
+              <v-icon>mdi-thumb-down</v-icon>
+            </v-btn>
+            <div>{{ post.dislike }}</div>
+          </v-row>
+        </v-list-item-content>
+      </v-card-title>
+      <pre>{{post.content}}</pre>
+    </v-card>
+
+    <v-list>
       <v-list-item
           v-for="reply in replies"
           :key="reply.reply_id"
+          style="margin-top: 20px;flex: auto"
       >
-        <v-icon large color="blue darken-2">mdi-message-text</v-icon>
-        <v-list-item-content style="padding-left: 10px" v-text="reply.content">
-        </v-list-item-content>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-btn icon color="deep-orange">
-          <v-icon>mdi-thumb-up</v-icon>
-        </v-btn>
-        <div>{{ reply.like }}</div>
-        <v-btn icon color="blue-grey darken-2">
-          <v-icon>mdi-thumb-down</v-icon>
-        </v-btn>
-        <div>{{ reply.dislike }}</div>
-        <v-list-item-content style="margin-right: 10px;margin-left: 20px">2022-10-15 10:04</v-list-item-content>
+        <v-card
+            class="mx-auto"
+            width="100%"
+        >
+          <v-card-title>
+            <v-list-item-avatar color="grey darken-3">
+              <v-img
+                  class="elevation-6"
+                  :src="reply.avatar"
+              ></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ reply.user_name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ reply.time }}</v-list-item-subtitle>
+              <v-row
+                  align="center"
+                  justify="end"
+                  style="margin-right: 20px;"
+              >
+                <v-btn icon color="deep-orange">
+                  <v-icon>mdi-thumb-up</v-icon>
+                </v-btn>
+                <div>{{ reply.like }}</div>
+                <v-btn icon color="blue-grey darken-2">
+                  <v-icon>mdi-thumb-down</v-icon>
+                </v-btn>
+                <div>{{ reply.dislike }}</div>
+              </v-row>
+            </v-list-item-content>
+          </v-card-title>
+          <pre>{{reply.content}}</pre>
+        </v-card>
       </v-list-item>
     </v-list>
+
     <v-card-text style="margin-top: 30px">以陈俊杰的身份参与讨论：</v-card-text>
     <mavon-editor
         v-model="newReply.content"
@@ -43,23 +95,23 @@
 <script>
 export default {
   name: "ReplyList",
-  props: ["replies"],
+  props: ["replies", "post"],
   data() {
     return {
       newReply: {
-        content:"",
-        html:""
+        content: "",
+        html: ""
       }
     }
   },
   methods: {
     // 所有操作都会被解析重新渲染
-    change(value, render){
+    change(value, render) {
       // render 为 markdown 解析后的结果[html]
       this.html = render;
     },
     // 提交
-    submit(){
+    submit() {
       console.log(this.newReply.content); //内容
       // console.log(this.newReply.html);
       /*
@@ -71,5 +123,12 @@ export default {
 </script>
 
 <style scoped>
-
+pre {
+  margin-left: 20px;
+  margin-bottom: 20px;
+  margin-right: 20px;
+  tab-size: 2;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
 </style>
--- bcc/src/pages/PostPage.vue
+++ bcc/src/pages/PostPage.vue
@@ -8,7 +8,7 @@
         <MyHeader></MyHeader>
       </el-header>
       <el-main>
-        <ReplyList :replies="replies"></ReplyList>
+        <ReplyList :replies="replies" :post="curPost"></ReplyList>
       </el-main>
     </el-container>
   </el-container>
@@ -24,16 +24,37 @@ export default {
   components: {ReplyList, SideBar, MyHeader},
   data() {
     return {
-      replies: [{
-        reply_id: 1,
+      /*
+      TODO: 通过路由参数获取post
+       */
+      curPost: {
+        post_id: 1,
+        avatar: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+        user_name: "陈俊杰",
+        title: "自动化测试",
         like: 22,
         dislike: 0,
+        time: "2022-12-20 12:40:20",
+        content: "省流不看版 ..."
+      },
+      replies: [{
+        reply_id: 1,
+        avatar: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+        user_name: "陈俊杰",
+        title: "自动化测试",
+        like: 22,
+        dislike: 8,
+        time: "2022-12-20 12:40:20",
         content: "省流不看版 ..."
       }, {
         reply_id: 2,
+        avatar: "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+        user_name: "陈俊杰",
+        title: "自动化测试",
         like: 22,
         dislike: 0,
-        content: "哈哈哈哈哈"
+        time: "2022-10-15 10:04:20",
+        content: "66666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666"
       }],
     }
   },
